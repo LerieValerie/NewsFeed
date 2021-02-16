ad8de88c76a0d1a39e26f3dc88bc91ac78f7ef0a
LerieValerie <valeriyasm91@mail.ru>
2021-02-16 11:52:53 +0300

    view binding

--- app/src/main/java/com/lerie_valerie/newsfeed/presentation/MainActivity.kt
+++ app/src/main/java/com/lerie_valerie/newsfeed/presentation/MainActivity.kt
@@ -2,12 +2,31 @@ package com.lerie_valerie.newsfeed.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.lerie_valerie.newsfeed.R
+import com.lerie_valerie.newsfeed.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        supportFragmentManager.findFragmentById(R.id.nav_host)?.findNavController()?.let { nav ->
+            appBarConfiguration = AppBarConfiguration(nav.graph)
+            setupActionBarWithNavController(nav, appBarConfiguration)
+        }
     }
+
+    override fun onSupportNavigateUp() =
+            NavigationUI.navigateUp(findNavController(R.id.nav_host), appBarConfiguration)
 }
--- app/src/main/java/com/lerie_valerie/newsfeed/presentation/roster/NewsFeedRosterFragment.kt
+++ app/src/main/java/com/lerie_valerie/newsfeed/presentation/roster/NewsFeedRosterFragment.kt
@@ -1,6 +1,42 @@
 package com.lerie_valerie.newsfeed.presentation.roster
 
+import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
+import com.lerie_valerie.newsfeed.databinding.FragmentNewsFeedRosterBinding
 
 class NewsFeedRosterFragment: Fragment() {
+    private lateinit var binding: FragmentNewsFeedRosterBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = FragmentNewsFeedRosterBinding.inflate(inflater, container, false)
+        .apply { binding = this }.root
+
+//    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+//        inflater.inflate(R.menu.actions, menu)
+//        super.onCreateOptionsMenu(menu, inflater)
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        when (item.itemId) {
+//            R.id.load -> {
+//                viewModel.reloadFromRemoteFailure()
+//                viewModel.load()
+//                return true
+//            }
+//            R.id.delete -> {
+//                viewModel.delete()
+//                return true
+//            }
+//        }
+//        return super.onOptionsItemSelected(item)
+//    }
 }
