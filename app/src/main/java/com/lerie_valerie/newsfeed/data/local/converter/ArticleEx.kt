93a5fe4afea249091fe884a2b730706e256c2e88
LerieValerie <valeriyasm91@mail.ru>
2021-03-10 13:33:18 +0300

    Code Format

--- app/src/main/java/com/lerie_valerie/newsfeed/data/local/converter/ArticleEx.kt
+++ app/src/main/java/com/lerie_valerie/newsfeed/data/local/converter/ArticleEx.kt
@@ -5,23 +5,23 @@ import com.lerie_valerie.newsfeed.domain.entity.Article
 import kotlinx.datetime.Instant
 
 fun Article.toModel() = ArticleModel(
-        id = id,
-        key = key,
-        title = title,
-        description = description,
-        date = date?.toLong(),
-        urlToImage = urlToImage,
-        url = url
+    id = id,
+    key = key,
+    title = title,
+    description = description,
+    date = date?.toLong(),
+    urlToImage = urlToImage,
+    url = url
 )
 
 fun ArticleModel.toEntity() = Article(
-        id = id,
-        key = key,
-        title = title,
-        description = description,
-        date = date?.toInstant(),
-        urlToImage = urlToImage,
-        url = url
+    id = id,
+    key = key,
+    title = title,
+    description = description,
+    date = date?.toInstant(),
+    urlToImage = urlToImage,
+    url = url
 )
 
 private fun Instant.toLong(): Long = this.toEpochMilliseconds()
